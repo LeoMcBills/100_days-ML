17df2fdc6721b068187901d89bb78181b529f166
LeoMcBills <leokinyera9@gmail.com>
2024-09-13 15:36:39 +0300

    pull

--- 2d_setup.py
+++ 2d_setup.py
@@ -1,1 +1,3 @@
 '''With the help of init_device_mesh(), we can accomplish the above 2D setup in just two lines, and we can still access the underlyng ProcessGroup if needed.'''
+from torch.distributed.device_mesh import init_device_mesh
+mesh_2d = init_device_mesh("cuda", ())
