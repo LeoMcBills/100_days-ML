e52f72263eec41137f3c22b75b4128b909ce7b85
LeoMcBills <leokinyera9@gmail.com>
2024-09-13 15:40:25 +0300

    rebased

--- 2d_setup.py
+++ 2d_setup.py
@@ -1,3 +1,6 @@
 '''With the help of init_device_mesh(), we can accomplish the above 2D setup in just two lines, and we can still access the underlyng ProcessGroup if needed.'''
 from torch.distributed.device_mesh import init_device_mesh
-mesh_2d = init_device_mesh("cuda", ())
+mesh_2d = init_device_mesh("cuda", (2, 4), mesh_dim_names=("replicate", "shard"))
+
+# Users can access the underlying process group through `get_group` API.
+replica
