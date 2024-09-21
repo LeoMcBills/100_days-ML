22534e59c0814090d87ba3249f2505835cb2c98e
LeoMcBills <leokinyera9@gmail.com>
2024-09-21 14:49:34 +0300

    modified out

--- c_plusplus/introAndDatatypes/datatypes2.cpp
+++ c_plusplus/introAndDatatypes/datatypes2.cpp
@@ -6,12 +6,12 @@ using namespace std;
 int main()
 {
 	// cout outputs integers as decimal integers:
-	cout << "Value of 0xFF = " << 0xFF << "decimal" << endl;	//output: 255 decimal
+	cout << "Value of 0xFF = " << 0xFF <<  " decimal" << endl;	//output: 255 decimal
 	
 	// The manipulator hex changes output to hexadecimal format
 	// (dec changes to decimal format):
 	
-	cout << "Value of 27 = " << hex << 27 << "hexadecimal" << endl; //output - hexadecimal
+	cout << "Value of 27 = " << hex << 27 << " hexadecimal" << endl; //output - hexadecimal
 
 	return 0;
 }
