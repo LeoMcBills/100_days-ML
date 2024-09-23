ea37ca1474ca8b5954211a343876d447465c27e9
LeoMcBills <leokinyera9@gmail.com>
2024-09-23 18:47:44 +0300

    fill preposfix

---
+++ c_plusplus/day3/preposfix.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i(2), j(8);
+
+    cout << i++ << endl;
+    cout << i << endl;
+    cout << j-- << endl;
+    cout << --j << endl;
+
+    return 0;
+}
