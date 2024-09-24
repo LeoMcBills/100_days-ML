274135f510c4918964e7c600fb1896ac596de99a
LeoMcBills <leokinyera9@gmail.com>
2024-09-24 11:09:05 +0300

    write for loop

--- c_plusplus/control_statements/forloop.cpp
+++ c_plusplus/control_statements/forloop.cpp
@@ -9,6 +9,13 @@ int main()
     double rate = 1.15;
 
     cout << fixed << setprecision(2);
+    cout << "\tEuro" << "\tDollar" << endl;
+    
+    for (int euro = 1; euro <= 5; ++euro)
+    {
+        double dollar = euro * rate;
+        cout << "\t" << euro << "\t" << dollar << endl;
+    }
 
     return 0;
 }
