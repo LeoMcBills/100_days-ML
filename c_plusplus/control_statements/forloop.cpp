70d5647114109f163b0480f0671960163dc2ebe7
LeoMcBills <leokinyera9@gmail.com>
2024-09-24 11:02:38 +0300

    dissecting program

---
+++ c_plusplus/control_statements/forloop.cpp
@@ -0,0 +1,14 @@
+// Write an exchange rate program
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    double rate = 1.15;
+
+    cout << fixed << setprecision(2);
+
+    return 0;
+}
