9a62d20e7a12a29e384ff05035d9d0870e151680
LeoMcBills <leokinyera9@gmail.com>
2024-09-24 15:14:51 +0300

    write break logic

--- c_plusplus/control_statements/ascii.cpp
+++ c_plusplus/control_statements/ascii.cpp
@@ -6,6 +6,27 @@ using namespace std;
 
 int main()
 {
+    int ac = 32, upper;
+    char answer;
 
+    while (true)
+    {
+        cout << "\nCharacter    Decimal     Hexadecimal\n\n" << endl;
+
+        for (upper = ac + 20; ac < upper && ac < 256; ++ac){
+            cout << "   " << (char)ac << setw(10) << "dec" << setw(10) << "hex" << ac << endl;
+        }
+
+        if (upper >= 256)   break;
+
+        cout << "\nGo on -> <return>, stop -> <q> + <return>" << endl;
+
+        cin.get(answer);
+        
+        if (answer == 'q' || answer == 'Q') break;
+        
+        cin.sync();
+    }
+    
     return 0;
 }
