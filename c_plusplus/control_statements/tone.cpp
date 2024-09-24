516ea0a23bdf67911b41192d295b9b23fb9bb131
LeoMcBills <leokinyera9@gmail.com>
2024-09-24 12:29:40 +0300

    do a do-while loop

---
+++ c_plusplus/control_statements/tone.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+const long delay = 10000000L;
+
+int main()
+{
+    int tic;
+
+    cout << "\nHow often should the tone be output? ";
+    cin >> tic;
+
+    do
+    {
+        for (long i = 0; i < delay; i++)
+        {
+            cout << "Now the tone!\a" << endl;
+        }
+    } while (--tic > 0)
+    {
+        cout << "End of the acoustic interlude!" << endl;
+    }
+
+    return 0;
+}
