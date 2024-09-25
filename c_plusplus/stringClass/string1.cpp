3247e94f602cd5ffd02add854bb82749f3ac0e9b
LeoMcBills <leokinyera9@gmail.com>
2024-09-25 13:35:24 +0300

    program structure

---
+++ c_plusplus/stringClass/string1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string prompt("Enter a line of text: "),
+        line(50, '*');      // Global strings
+
+int main()
+{
+
+    return 0;
+}
