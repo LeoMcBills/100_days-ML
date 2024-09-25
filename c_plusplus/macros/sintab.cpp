4ae89ceb74780141d1011e5f7c123773e428d70f
LeoMcBills <leokinyera9@gmail.com>
2024-09-25 10:54:30 +0300

    write program

---
+++ c_plusplus/macros/sintab.cpp
@@ -0,0 +1,19 @@
+// Creates a sine function table
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+#define PI          3.1415926536
+#define START       0.0
+#define END         (2.0 * PI)
+#define STEP        (PI / 8.0)
+#define HEADER      (cout << \
+        "  *****  Sine Function Table  *****\n\n")
+
+int main()
+{
+
+    return 0;
+}
