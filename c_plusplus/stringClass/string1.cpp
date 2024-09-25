3c483b6fcccff984bd3dafe94444941979ff4644
LeoMcBills <leokinyera9@gmail.com>
2024-09-25 13:43:30 +0300

    string manipulation

--- c_plusplus/stringClass/string1.cpp
+++ c_plusplus/stringClass/string1.cpp
@@ -7,6 +7,21 @@ string prompt("Enter a line of text: "),
 
 int main()
 {
+    string text;
+    cout << line << endl << prompt << endl;
+    getline(cin, text);
+    cout << line << endl
+            << "Your text is " << text.size()
+            << " characters long!" << endl;
 
+    string copy(text),
+            start(text,0,10);
+
+    cout << "Your text:\n" << copy << endl;
+    text = "1234567890";
+    cout << line << endl
+            << "The first 10 characters:\n" << start << endl
+            << text << endl;
+    
     return 0;
 }
