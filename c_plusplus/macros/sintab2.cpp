5edcc5c4a2e820930e404e72f7296135a58cfb69
LeoMcBills <leokinyera9@gmail.com>
2024-09-25 11:18:28 +0300

    figure out

---
+++ c_plusplus/macros/sintab2.cpp
@@ -0,0 +1,24 @@
+// Creates a sine function table
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+#define PI          3.1415926536
+#define START       0.0
+#define END         (2.0 * PI)
+#define STEP        (PI / 8.0)
+#define HEADER      (cout << "\n *************** Sine Function Table *************\n")
+
+int main()
+{
+    double x;
+
+    HEADER;
+
+    std::cout << setw(16) << "x" << setw(20) << "sin(x)\n" << endl;
+    std::cout << std::setw(20) << x << std::setw(16) << sin(x) << endl;
+
+    return 0;           
+
+}
