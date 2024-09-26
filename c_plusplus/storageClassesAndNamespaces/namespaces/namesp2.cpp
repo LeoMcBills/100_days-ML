2f357491fbd5545c04acf0e26e82fd04e6cb6ff1
LeoMcBills <leokinyera9@gmail.com>
2024-09-26 16:17:14 +0300

    use namespaces

--- c_plusplus/storageClassesAndNamespaces/namespaces/namesp2.cpp
+++ c_plusplus/storageClassesAndNamespaces/namespaces/namesp2.cpp
@@ -1,2 +1,46 @@
 // Demonstrates the use of using-declarations and using-directives.
 
+#include <iostream>         // Namespace std is defined here.
+
+void message()              // Global functon :: message()
+{
+    std::cout << "Within function :: message()\n";
+}
+
+namespace A
+{
+    using namespace std;     // Names of std are visible here
+    void message()
+    {
+        cout << "Within function A::message()\n";
+    }
+}
+
+namespace B
+{
+    using std::cout;        // Declaring cout of std.
+    void message(void);     // Function B::message()
+}
+
+void B::message(void)       // Defining B::message()
+{
+    cout << "Within function B::message()\n";
+}
+
+int main()
+{
+    using namespace std;    // Names of namespace std
+    using B::message;       // Function name without braces!
+
+    cout << "Testing namespaces!\n";
+    cout << "\nCall of A::message()" << endl;
+
+    A::message();
+    cout << "\nCall of B::message()" << endl;
+
+    message();              // ::message() is hidden because of the using-declaration.
+    cout << "\nCall of::message()" << endl;
+    ::message();            // Global function
+    
+    return 0;
+}
