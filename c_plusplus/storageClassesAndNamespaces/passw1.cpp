abfb1b855946a80f86a18d1075b2c0ea575390d0
LeoMcBills <leokinyera9@gmail.com>
2024-09-26 13:27:46 +0300

    write func

---
+++ c_plusplus/storageClassesAndNamespaces/passw1.cpp
@@ -0,0 +1,54 @@
+// The functions getPassword() and timediff() to read and examine a password
+
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+#include <string>
+using namespace std;
+
+long timediff(void);
+static string secret = "ISUS";
+static long maxcount = 3, maxtime = 60;
+
+bool getPassword()
+{
+    bool ok_flag = false;
+    string word;
+    int count = 0, time = 0;
+    timediff();
+
+    while ( ok_flag != true && ++count <= maxcount)
+    {
+        cout << "\n\nInput the password: ";
+        cin.sync();
+        cin >> setw(20) >> word;
+        time += timediff();
+
+        if ( time >= maxtime )
+        {
+            cout << "Time expired\n";
+            break;
+        }
+
+        if ( word != secret )
+        {
+            cout << "Wrong password\n";
+            cout << "You have " << maxcount - count << " more tries\n";
+        }
+        else
+        {
+            cout << "Correct password\n";
+            ok_flag = true;
+        }
+    }
+
+    return ok_flag;
+}
+
+long timediff()
+{
+    static long sec = 0;
+    long oldsec = sec;
+    time( &sec);
+    return (sec - oldsec)
+}
