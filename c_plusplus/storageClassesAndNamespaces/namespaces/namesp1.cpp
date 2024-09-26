0dade2ba426df02721ec8a6502d125afeecc13b1
LeoMcBills <leokinyera9@gmail.com>
2024-09-26 15:14:58 +0300

    create own namespace

---
+++ c_plusplus/storageClassesAndNamespaces/namespaces/namesp1.cpp
@@ -0,0 +1,10 @@
+// Defines and tests namespaces.
+// ---------------------------------------------------
+#include <string>
+
+namespace MySpace
+{
+    std::string mess = "Within namespace MySpace";
+    int count = 0;
+    double f( double); 
+}
