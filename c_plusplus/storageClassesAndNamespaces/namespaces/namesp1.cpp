571f98137f452e265169ba544cbb64dcd525eff3
LeoMcBills <leokinyera9@gmail.com>
2024-09-26 15:19:44 +0300

    yourspace namespace

--- c_plusplus/storageClassesAndNamespaces/namespaces/namesp1.cpp
+++ c_plusplus/storageClassesAndNamespaces/namespaces/namesp1.cpp
@@ -8,3 +8,13 @@ namespace MySpace
     int count = 0;          // Definition: MySpace::count
     double f( double);      // Prototype:  MySpace::f()
 }
+
+namespace YourSpace
+{
+    std::string mess = "Within namespace YourSpace";
+    void f()
+    {
+        mess += '!';        // Definition: YourSpace::mess
+    }
+}
+
