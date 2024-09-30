04b77a44fc5708ec23b42c3472f4eb8309124b18
LeoMcBills <leokinyera9@gmail.com>
2024-09-30 17:11:52 +0300

    write basic class def and use

--- c_plusplus/classes/classDejaVu.cpp
+++ c_plusplus/classes/classDejaVu.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 
 class Car {
+    public:
+        string brand;
+        int year;
 
+        void display() {
+            cout << "Brand: " << brand << ", Year: " << year << endl;
+        }
 };
+
+int main() {
+    Car myCar;
+    myCar.brand = "Range Rover Autobiography";
+    myCar.year = 2020;
+    myCar.display();
+
+    return 0;
+}
