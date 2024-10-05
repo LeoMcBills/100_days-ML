0565f5b7b7a2d49956194c50471dabf4104093bd
LeoMcBills <leokinyera9@gmail.com>
2024-10-05 15:37:43 +0300

    main function

--- c_plusplus/prac/prac1.cpp
+++ c_plusplus/prac/prac1.cpp
@@ -8,17 +8,11 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
-#include <iostream>
-#include <cstdlib>
-#include <string>
-#include <cstring>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <netdb.h>
 
-
+int main(int argc, char **argv) {
+    
+    return 0;
+}
 
 int main(int argc, char **argv) {
   // Flush after every std::cout / std::cerr
