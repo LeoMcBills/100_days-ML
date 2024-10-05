0b8636bcee13837e5a88dbca8a40ec8657f9307b
LeoMcBills <leokinyera9@gmail.com>
2024-10-05 16:10:26 +0300

    create server socket

--- c_plusplus/prac/server.cpp
+++ c_plusplus/prac/server.cpp
@@ -5,6 +5,14 @@
 #include <cstring>
 
 int main() {
+    // Create the server socket
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0) {
+        std::cerr << "Failed to create server socket\n";
+        return 1;
+    }
+
+    
 
     return 0;
 }
