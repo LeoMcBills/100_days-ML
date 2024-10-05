77067db08c6da1ed7f651b79743fbf54d773dfe6
LeoMcBills <leokinyera9@gmail.com>
2024-10-05 16:14:39 +0300

    define server address

--- c_plusplus/prac/server.cpp
+++ c_plusplus/prac/server.cpp
@@ -12,7 +12,12 @@ int main() {
         return 1;
     }
 
-    
+    // Define the server address
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;  // Bind to all available interfaces
+    address.sin_port = htons(8080);        // Use port 8080
 
     return 0;
 }
