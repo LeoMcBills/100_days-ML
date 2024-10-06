d336efb6b0da547ea6c445bc80c4f5425531a2cd
LeoMcBills <leokinyera9@gmail.com>
2024-10-06 15:04:04 +0300

    write training script

---
+++ models/train.py
@@ -0,0 +1,39 @@
+import gc
+from data import train_loader, device
+import torch
+from hyperparameters import num_epochs, num_classes, batch_size, learning_rate, model, criterion, optimizer
+
+total_step = len(train_loader)
+
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        # Move tensors to the configured device
+        images = images.to(device)
+        labels = labels.to(device)
+
+        # Forward pass
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+
+        # Backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        del images, labels, outputs
+        torch.cuda.empty_cache()
+        gc.collect()
+    print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch+1, num_epochs, loss.item()))
+
+    #Validation
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for images, labels in valid_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+            del images, labels, outputs
+        print('Accuracy of the network on the {} validation images: {} %'.format(5000, 100 * correct / total))
