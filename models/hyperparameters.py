b54cad801143de5b37f23dba0404b776c13b2d5e
LeoMcBills <leokinyera9@gmail.com>
2024-10-06 13:36:01 +0300

    hyperparameter

---
+++ models/hyperparameters.py
@@ -0,0 +1,19 @@
+import torch.nn as nn
+from resnet import ResNet, ResidualBlock
+import torch
+from data import device
+from data import train_loader
+
+num_classes = 10
+num_epochs = 10
+batch_size = 16
+learning_rate = 0.01
+
+model = ResNet(ResidualBlock, [3, 4, 6, 3]).to(device)
+
+# Loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr==learning_rate, weight_decay=0.001, momentum=0.9)
+
+# Train the model
+total_step = len(train_loader)
--- models/resnet.py
+++ models/resnet.py
@@ -33,6 +33,7 @@ class ResidualBlock(Module):
 class ResNet(Module):
     def __init__(self, block, layers, num_classes=10):
         super(ResNet, self).__init__()
+        self.inplanes = 64
         self.conv1 = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3),
             nn.BatchNorm2d(64),
@@ -43,12 +44,12 @@ class ResNet(Module):
         self.layer1 = self._make_layer(block, 128, layers[1], stride=2)
         self.layer2 = self._make_layer(block, 256, layers[2], stride=2)
         self.layer3 = self._make_layer(block, 512, layers[3], stride=2)
-        self.avgpool = nn.AvfPool2d(7, stride=1)
+        self.avgpool = nn.AvgPool2d(7, stride=1)
         self.fc = nn.Linear(512, num_classes)
 
     def _make_layer(self, block, planes, blocks, stride=1):
         downsample = None
-        if stride != 1 or self.inplanees != planes:
+        if stride != 1 or self.inplanes != planes:
             downsample = nn.Sequential(
                 nn.Conv2d(self.inplanes, planes, kernel_size=1, stride=stride),
                 nn.BatchNorm2d(planes),
