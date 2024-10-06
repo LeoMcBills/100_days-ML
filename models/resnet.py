2f9b0ab4ba1d635d2fff722133ae7be943e92cf0
LeoMcBills <leokinyera9@gmail.com>
2024-10-06 11:55:00 +0300

    data handling function

--- models/resnet.py
+++ models/resnet.py
@@ -15,6 +15,7 @@ def data_loader(data_dir,
                 random_seed=42,
                 valid_size=0.1,
                 shuffle=True,
+                train=True,
                 test=False):
     normalize = transforms.Normalize(
         mean=[0.4914, 0.4822, 0.4465],
@@ -43,4 +44,41 @@ def data_loader(data_dir,
 
         return data_loader
     
-    
+    # Load the dataset
+    train_dataset = datasets.CIFAR10(
+        root=data_dir,
+        train=train,
+        download=True,
+        transform=transform,
+    )
+
+    valid_dataset = datasets.CIFAR10(
+        root=data_dir,
+        train=train,
+        download=True,
+        transform=transform,
+    )
+
+    num_train = len(train_dataset)
+    indices = list(range(num_train))
+    split = int(np.floor(valid_size * num_train))
+
+    if shuffle:
+        np.random.seed(42)
+        np.random.shuffle(indices)
+
+    train_idx, valid_idx = indices[split:], indices[:split]
+    train_sampler = SubsetRandomSampler(train_idx)
+    valid_sampler = SubsetRandomSampler(valid_idx)
+
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        sampler=valid_sampler,
+    )
+
+    valid_loader = torch.utils.data.DataLoader(
+        valid_dataset, batch_size=batch_size, sampler=valid_sampler
+    )
+
+    return (train_loader, valid_loader)
