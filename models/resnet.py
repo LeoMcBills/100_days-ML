97b1b4dcce5a56128b8524e3ac8b018320f10259
LeoMcBills <leokinyera9@gmail.com>
2024-10-06 12:43:44 +0300

    write init block

--- models/resnet.py
+++ models/resnet.py
@@ -1,7 +1,20 @@
 import torch
 from torch.nn import Module
+import torch.nn as nn
 import numpy as np
 
 class ResidualBlock(Module):
     def __init__(self, in_channels, out_channels, stride=1, downsample=None):
         super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU()
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=stride, padding=1),
+            nn.BatchNorm2d(out_channels)
+        )
+        self.downsample = downsample
+        self.relu = nn.ReLU()
+        self.out_channels = out_channels
