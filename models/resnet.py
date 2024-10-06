c0fd8bb2fcd66d8eb8ff7ca35d1b11cdd56bc63d
LeoMcBills <leokinyera9@gmail.com>
2024-10-06 11:41:15 +0300

    load test dataset

--- models/resnet.py
+++ models/resnet.py
@@ -10,3 +10,37 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # print('Device:', device)
 
 # Data handling
+def data_loader(data_dir,
+                batch_size,
+                random_seed=42,
+                valid_size=0.1,
+                shuffle=True,
+                test=False):
+    normalize = transforms.Normalize(
+        mean=[0.4914, 0.4822, 0.4465],
+        std=[0.2023, 0.1994, 0.2010],
+    )
+    # Define transforms
+    transform = transforms.compose([
+        transforms.Resize((224,224)),
+        transforms.ToTensor(),
+        normalize,
+    ])
+
+    if test:
+        dataset = datasets.CIFAR10(
+            root=data_dir,
+            train=False,
+            download=True,
+            transform=transform,
+        )
+
+        data_loader = torch.utils.data.DataLoader(
+            dataset,
+            batch_size=batch_size,
+            shuffle=shuffle,
+        )
+
+        return data_loader
+    
+    
