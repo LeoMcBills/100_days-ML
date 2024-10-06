6d3ff13b7483b0b89af327ca95779886ac3273c7
LeoMcBills <leokinyera9@gmail.com>
2024-10-06 12:19:27 +0300

    residual block

--- models/resnet.py
+++ models/resnet.py
@@ -2,3 +2,6 @@ import torch
 from torch.nn import Module
 import numpy as np
 
+class ResidualBlock(Module):
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
+        super(ResidualBlock, self).__init__()
