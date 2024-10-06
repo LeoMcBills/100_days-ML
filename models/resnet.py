d745678af057e18f39e9a874762fc2783febe06f
LeoMcBills <leokinyera9@gmail.com>
2024-10-06 11:07:26 +0300

    building block

---
+++ models/resnet.py
@@ -0,0 +1,8 @@
+import numpy as np
+import torch
+import torch.nn as nn
+from torchvision import datasets, transforms
+from torch.utils.data.sampler import SubsetRandomSampler
+
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
