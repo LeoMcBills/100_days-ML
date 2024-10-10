f1cfb6f9b672021f2cc983ace9795752a8ee84d8
LeoMcBills <leokinyera9@gmail.com>
2024-10-10 12:52:00 +0300

    back to basics

--- mri3d.py
+++ mri3d.py
@@ -1,57 +1,45 @@
 # Import data
-import sys
 import time
 import numpy as np
+
 from skimage import io
+
+vol = io.imread("https://s3.amazonaws.com/assets.datacamp.com/blog_assets/attention-mri.tif")
+volume = vol.T
+r, c = volume[0].shape
+
+# Define frames
 import plotly.graph_objects as go
-from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
-from plotly.subplots import make_subplots
-from plotly.graph_objs import Figure
-from plotly.offline import plot
+nb_frames = 68
 
-class MRIViewer(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle('MRI Viewer')
-        self.setGeometry(100, 100, 800, 600)
+fig = go.Figure(frames=[go.Frame(data=go.Surface(
+    z=(6.7 - k * 0.1) * np.ones((r, c)),
+    surfacecolor=np.flipud(volume[67 - k]),
+    cmin=0, cmax=200
+    ),
+    name=str(k) # you need to name the frame for the animation to behave properly
+    )
+    for k in range(nb_frames)])
 
-        central_widget = QWidget()
-        self.setCentralWidget(central_widget)
-        layout = QVBoxLayout(central_widget)
+# Add data to be displayed before animation starts
+fig.add_trace(go.Surface(
+    z=6.7 * np.ones((r, c)),
+    surfacecolor=np.flipud(volume[67]),
+    colorscale='Gray',
+    cmin=0, cmax=200,
+    colorbar=dict(thickness=20, ticklen=4)
+    ))
 
-        self.vol = io.imread("https://s3.amazonaws.com/assets.datacamp.com/blog_assets/attention-mri.tif")
-        self.volume = self.vol.T
-        self.r, self.c = self.volume[0].shape
 
-        self.fig = self.create_figure()
-        html = plot(self.fig, output_type='div', include_plotlyjs='cdn')
-        
-        from PyQt5.QtWebEngineWidgets import QWebEngineView
-        web_view = QWebEngineView()
-        web_view.setHtml(html)
-        layout.addWidget(web_view)
+def frame_args(duration):
+    return {
+            "frame": {"duration": duration},
+            "mode": "immediate",
+            "fromcurrent": True,
+            "transition": {"duration": duration, "easing": "linear"},
+        }
 
-    def create_figure(self):
-        nb_frames = 68
-
-        fig = go.Figure(frames=[go.Frame(data=go.Surface(
-            z=(6.7 - k * 0.1) * np.ones((self.r, self.c)),
-            surfacecolor=np.flipud(self.volume[67 - k]),
-            cmin=0, cmax=200
-            ),
-            name=str(k)
-            )
-            for k in range(nb_frames)])
-
-        fig.add_trace(go.Surface(
-            z=6.7 * np.ones((self.r, self.c)),
-            surfacecolor=np.flipud(self.volume[67]),
-            colorscale='Gray',
-            cmin=0, cmax=200,
-            colorbar=dict(thickness=20, ticklen=4)
-            ))
-
-        sliders = [
+sliders = [
             {
                 "pad": {"b": 10, "t": 60},
                 "len": 0.9,
@@ -59,7 +47,7 @@ class MRIViewer(QMainWindow):
                 "y": 0,
                 "steps": [
                     {
-                        "args": [[f.name], self.frame_args(0)],
+                        "args": [[f.name], frame_args(0)],
                         "label": str(k),
                         "method": "animate",
                     }
@@ -68,50 +56,37 @@ class MRIViewer(QMainWindow):
             }
         ]
 
-        fig.update_layout(
-            title='Slices in volumetric data @ MRI-Uganda',
-            width=600,
-            height=600,
-            scene=dict(
-                zaxis=dict(range=[-0.1, 6.8], autorange=False),
-                aspectratio=dict(x=1, y=1, z=1),
-            ),
-            updatemenus = [
-                {
-                    "buttons": [
-                        {
-                            "args": [None, self.frame_args(50)],
-                            "label": "&#9654;", # play symbol
-                            "method": "animate",
-                        },
-                        {
-                            "args": [[None], self.frame_args(0)],
-                            "label": "&#9724;", # pause symbol
-                            "method": "animate",
-                        },
-                    ],
-                    "direction": "left",
-                    "pad": {"r": 10, "t": 70},
-                    "type": "buttons",
-                    "x": 0.1,
-                    "y": 0,
-                }
-            ],
-            sliders=sliders
-        )
+# Layout
+fig.update_layout(
+         title='Slices in volumetric data @ MRI-Uganda',
+         width=600,
+         height=600,
+         scene=dict(
+                    zaxis=dict(range=[-0.1, 6.8], autorange=False),
+                    aspectratio=dict(x=1, y=1, z=1),
+                    ),
+         updatemenus = [
+            {
+                "buttons": [
+                    {
+                        "args": [None, frame_args(50)],
+                        "label": "&#9654;", # play symbol
+                        "method": "animate",
+                    },
+                    {
+                        "args": [[None], frame_args(0)],
+                        "label": "&#9724;", # pause symbol
+                        "method": "animate",
+                    },
+                ],
+                "direction": "left",
+                "pad": {"r": 10, "t": 70},
+                "type": "buttons",
+                "x": 0.1,
+                "y": 0,
+            }
+         ],
+         sliders=sliders
+)
 
-        return fig
-
-    def frame_args(self, duration):
-        return {
-            "frame": {"duration": duration},
-            "mode": "immediate",
-            "fromcurrent": True,
-            "transition": {"duration": duration, "easing": "linear"},
-        }
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    viewer = MRIViewer()
-    viewer.show()
-    sys.exit(app.exec_())
+fig.show()
